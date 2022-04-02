fc4ca6e360f4ad05e55cae7d75dbd3c90f9befba
Ikhwan Maulana Akbar <43781583+ikhwanma@users.noreply.github.com>
2022-04-02 23:11:16 +0700

    Sampai coding exercise

---
+++ app/src/main/java/ikhwan/binar/binarchallengeempat/ui/fragment/SplashFragment.kt
@@ -0,0 +1,31 @@
+package ikhwan.binar.binarchallengeempat.ui.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import ikhwan.binar.binarchallengeempat.R
+
+
+class SplashFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed(Runnable {
+            Navigation.findNavController(view).navigate(R.id.action_splashFragment_to_loginFragment)
+        }, 2000)
+    }
+}
